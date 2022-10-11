2d09171ec76f751e52df242fac02cf3aef7f5f77
Andrei-Agmata <aagmata@myseneca.ca>
2022-10-11 16:15:28 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import { React, useEffect, useState } from "react";
 
 import Home from "./components/Home";
 import About from "./components/About";
@@ -8,14 +8,30 @@ import NavigationBar from "./components/NavigationBar";
 import Contact from "./components/Contact";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [exit, setExit] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setExit("loader-exit");
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }, 5000);
+  }, []);
   return (
-    <>
-      <NavigationBar />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
-    </>
+    <div className="App">
+      {loading ? (
+        <div className={`loader ${exit}`}>
+          <h1 className="loading-logo fw-bold">Aa.</h1>
+        </div>
+      ) : (
+        <>
+          <NavigationBar /> <Home /> <About /> <Projects /> <Contact />
+        </>
+      )}
+    </div>
   );
 }
 
